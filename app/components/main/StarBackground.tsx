5f32393fdf787bccf9f616bda46fc441db6ae6a4
Shreyansh99 <shreyanshkumarbasak@gmail.com>
2024-09-28 18:19:38 +0530

    fixed some ts errors

--- app/components/main/StarBackground.tsx
+++ app/components/main/StarBackground.tsx
@@ -1,11 +1,14 @@
 "use client"
 import React,{useState,useRef,Suspense} from 'react'
 import {Canvas, useFrame} from '@react-three/fiber'
-import {Points, PointMaterial, Preload} from '@react-three/drei'
+import {Points, PointMaterial} from '@react-three/drei'
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+//  @ts-expect-error
 import * as random from 'maath/random/dist/maath-random.esm'
 
-const StarBackground = (props: any) => {
-    const ref : any = useRef()
+const StarBackground = (props:unknown) => {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const ref:any = useRef()
     const [sphere] = useState(()=>
         random.inSphere(new Float32Array(5000), {radius : 1.2}))
 
