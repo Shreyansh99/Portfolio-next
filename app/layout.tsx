48465896efa0fdd60f01195601bfd5fc9e6ba301
Shreyansh99 <shreyanshkumarbasak@gmail.com>
2024-09-28 16:05:45 +0530

    Added Star Background Component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import StarsCanvas from "./components/main/StarBackground";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -28,6 +29,7 @@ export default function RootLayout({
       <body
         className={`bg-[#030014] overflow-y-scroll overflow-x-hidden ${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <StarsCanvas/>
         {children}
       </body>
     </html>
